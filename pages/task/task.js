3a5160a60d5aff3b5913b03a09f4093a07c4ca6f
xiaojin <xiaojinwhu10@gmail.com>
2017-06-26 23:16:54 +0800

    new tabbar

--- pages/task/task.js
+++ pages/task/task.js
@@ -1,100 +1,27 @@
 // pages/task/task.js
+var sliderWidth=96
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-    winWidth: 0,
-    winHeight: 0,
-    currentTab: 0,
-  
+    tabs: ["待录入", "已完成"],
+    activeIndex: 0,
+    sliderOffset: 0,
+    sliderLeft: 0,
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
   onLoad: function () {
-    var that = this; 
+    var that = this;
     wx.getSystemInfo({
-
       success: function (res) {
         that.setData({
-          winWidth: res.windowWidth,
-          winHeight: res.windowHeight
+          sliderLeft: (res.windowWidth / that.data.tabs.length - sliderWidth) / 2,
+          sliderOffset: res.windowWidth / that.data.tabs.length * that.data.activeIndex
         });
       }
-
-    });  
+    });
   },
-
-
-  bindChange: function (e) {
-
-    var that = this;
-    that.setData({ currentTab: e.detail.current });
-
-  },  
-
-  swichNav: function (e) {
-
-    var that = this;
-
-    if (this.data.currentTab === e.target.dataset.current) {
-      return false;
-    } else {
-      that.setData({
-        currentTab: e.target.dataset.current
-      })
-    }
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
+  tabClick: function (e) {
+    this.setData({
+      sliderOffset: e.currentTarget.offsetLeft,
+      activeIndex: e.currentTarget.id
+    });
   }
-})
+});
